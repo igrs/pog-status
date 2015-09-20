18f3af92230b1fba9038b1fe1c8262c6eecf6cf5
igrs <specialweekisno1@gmail.com>
2015-09-20 16:25:12 +0900

    display horse information.

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,39 @@ fn main() {
                     let mut body_bytes = vec![];
                     if res.read_to_end(&mut body_bytes).is_ok() {
                         let html = EUC_JP.decode(&body_bytes, DecoderTrap::Strict).unwrap();
-                        println!("{}", &html);
                         let doc = Html::from_string(html).parse();
+                        // horse name
+                        for h1 in doc.select("h1").unwrap() {
+                            let h1_node = h1.as_node();
+                            let text_node = h1_node.first_child().unwrap();
+                            let text = text_node.as_text().unwrap().borrow();
+                            println!("{}", text.to_string());
+                        }
+                        // profile
+                        for table in doc.select("table.db_prof_table").unwrap() {
+                            let trs = table.as_node().select("tr").unwrap();
+                            for tr in trs {
+                                let tr_node = tr.as_node();
+
+                                let th = tr_node.select("th").unwrap().nth(0).unwrap();
+                                let td = tr_node.select("td").unwrap().nth(0).unwrap();
+
+                                let th_node = th.as_node();
+                                let td_node = td.as_node();
+
+                                if !th_node.first_child().is_none() && !td_node.first_child().is_none() {
+                                    let th_text_node = th_node.first_child().unwrap();
+                                    let td_text_node = td_node.first_child().unwrap();
+
+                                    if !th_text_node.as_text().is_none() && !td_text_node.as_text().is_none() {
+                                        let th_text = th_text_node.as_text().unwrap().borrow();
+                                        let td_text = td_text_node.as_text().unwrap().borrow();
+                                        println!("{} -> {}", th_text.to_string(), td_text.to_string());
+                                    }
+                                }
+
+                            }
+                        }
                     }
                     else {
                         println!("Can not read response body.");
