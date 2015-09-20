cd735b97a8aa17d221a698cc3367b7d457900096
igrs <specialweekisno1@gmail.com>
2015-09-20 12:22:41 +0900

    get html body

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 extern crate csv;
+extern crate encoding;
+extern crate hyper;
+extern crate kuchiki;
 extern crate rustc_serialize;
 
+use std::io::Read;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::env;
 use std::fs::File;
 
-//static SITE_URL: &'static str = "http://www.netkeiba.com/horse/";
+use encoding::{Encoding, DecoderTrap};
+use encoding::all::EUC_JP;
+
+use hyper::Client;
+
+use kuchiki::Html;
+
+static SITE_URL: &'static str = "http://db.netkeiba.com/horse/";
 
 fn main() {
     let paths: Vec<String> = env::args().collect();
+
     if paths.len() > 1 {
+
+        let client = Client::new();
+
         for i in 1..paths.len() {
             let path = &paths[i];
             let file = match File::open(path) {
@@ -22,7 +37,22 @@ fn main() {
             };
             let buf = BufReader::new(file);
             for line in buf.lines().filter_map(|result| result.ok()) {
-                println!("{}", line);
+                if !&line.is_empty() {
+                    let url = SITE_URL.to_string() + &line;
+                    let mut res = client.get(&url)
+                                        .send()
+                                        .unwrap();
+                    let mut body_bytes = vec![];
+                    if res.read_to_end(&mut body_bytes).is_ok() {
+                        let html = EUC_JP.decode(&body_bytes, DecoderTrap::Strict).unwrap();
+                        println!("{}", &html);
+                        let doc = Html::from_string(html).parse();
+                    }
+                    else {
+                        println!("Can not read response body.");
+                    }
+
+                }
             }
         }
     }
